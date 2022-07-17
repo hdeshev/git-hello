8a6f981da568398b523f69087d55f42f62716fdc
Hristo Deshev <hristo@deshev.com>
2022-07-17 17:17:57 +0300

    Initial project commit
    Go module and main file.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func main() {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Fruit", "Amount"})
+	table.Append([]string{"apples", "10"})
+	table.Append([]string{"pears", "30"})
+	table.Append([]string{"raspberries", "3"})
+	table.Append([]string{"raspberries", "3"})
+	table.Render()
+}
